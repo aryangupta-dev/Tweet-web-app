af995b6e38c84e47daadac223033f4a2be1e0d8f
Aryan gupta <guptaaryan2468@gmail.com>
2021-08-09 14:35:50 +0530

    <dependency added to useEffect>

--- client/tailwindreact-app/src/pages/Post.jsx
+++ client/tailwindreact-app/src/pages/Post.jsx
@@ -22,7 +22,7 @@ function Post() {
     axios.get(`http://localhost:3001/comments/${id}`).then((response)=>{
       setComments(response.data);
     })
-  }, []);
+  }, [newComment]);
   return (
     <div className="flex flex-row ">
       <div className="w-1/2 min-h-screen bg-gray-900">
@@ -43,16 +43,16 @@ function Post() {
       </div>
       <div className="w-1/2 min-h-screen ">
         <h1 className="mt-2 ml-2 font-mono text-2xl text-gray-700">Comment Section</h1>
-        <div className="flex flex-col ">
+        <div className="flex flex-col items-center">
             {comments.map((comment,key)=>{
               return(
-                <div key={key}>{comment.comment}</div>
+                <div key={key}  className="px-5 py-2 m-2 bg-gray-500">{comment.comment}</div>
               );
 
             })}
         </div>
         <div className="fixed flex flex-row justify-around mt-8 bottom-5">
-          <input className="px-4 py-2 ml-4 mr-3 border border-gray-500 rounded-md" placeholder="Comment..." onChange={(event)=> {
+          <input className="px-8 py-2 ml-4 mr-3 border border-gray-500 rounded-md" placeholder="Comment..." onChange={(event)=> {
               setNewComment(event.target.value);
             }} value={newComment} type="text"></input>
           <button className="px-3 py-2 text-white bg-blue-600 rounded-md " type="submit" onClick={addComment}>Add comment</button>
